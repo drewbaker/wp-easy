7dc1f4a3e42c451d4481b56ea55a7563c5ac1f07
rsm0128 <rsm0128@hotmail.com>
2024-09-18 06:20:50 -0400

    removed theme related function

--- includes/class-template.php
+++ includes/class-template.php
@@ -24,13 +24,10 @@ class Template {
 		// Register our custom query var
 		add_filter( 'query_vars', array( $this, 'query_vars' ) );
 
-		add_action( 'wp_head', array( $this, 'print_importmaps' ) );
 		add_action( 'wp_footer', array( $this, 'print_component_scripts' ) );
 
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 10 );
-
-		add_action( 'the_post', array( $this, 'filter_post' ) );
-		add_action( 'the_posts', array( $this, 'filter_posts' ) );
+		add_action( 'the_post', array( $this, 'expand_post' ) );
+		add_action( 'the_posts', array( $this, 'expand_posts' ) );
 	}
 
 	/**
@@ -69,50 +66,11 @@ class Template {
 	}
 
 	/**
-	 * Enqueue Custom Scripts.
-	 */
-	public function enqueue_scripts() {
-		wp_enqueue_script( 'jquery' );
-
-		// Enqueue all JS files in /js/libs
-		$this->auto_enqueue_libs();
-
-		// Enqueue wp-easy scripts
-		wp_enqueue_script_module( 'main', get_theme_file_uri() . '/scripts/main.js', [ 'jquery' ], [], null, true );
-		wp_enqueue_script_module( 'fonts', get_theme_file_uri() . '/scripts/fonts.js', [], null, true );
-
-		// Setup JS variables in scripts
-		wp_localize_script(
-			'jquery',
-			'serverVars',
-			array(
-				'themeURL' => get_template_directory_uri(),
-				'homeURL'  => home_url(),
-			)
-		);
-	}
-
-	/**
-	 * Helper function to enqueue all JS files in /js/libs
-	 */
-	private function auto_enqueue_libs() {
-		$libs_dir = get_template_directory() . '/scripts/libs/';
-		$libs     = glob( $libs_dir . '*.js' );
-		foreach ( $libs as $lib ) {
-			// Remove file extension and version numbers for the handle name of the script
-			$handle = basename( $lib, '.js' );
-			$handle = str_replace( [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'js', '..' ], '', $handle );
-			$handle = rtrim( $handle, '.' );
-			wp_enqueue_script( $handle, get_theme_file_uri() . '/scripts/libs/' . basename( $lib ), [], null, [] );
-		}
-	}
-
-	/**
 	 * Filter an array of posts to add some default values to each post object.
 	 *
 	 * @param \WP_Post[] $posts Posts array.
 	 */
-	public function filter_posts( $posts ) {
+	public function expand_posts( $posts ) {
 		foreach ( $posts as $post ) {
 			$post = Utils::expand_post_object( $post );
 		}
@@ -124,40 +82,7 @@ class Template {
 	 *
 	 * @param \WP_Post $post
 	 */
-	public function filter_post( $post ) {
+	public function expand_post( $post ) {
 		$post = Utils::expand_post_object( $post );
-	}
-
-	/**
-	 * Adding JS moudle importmaps to the head, allows easier naming of JS imports.
-	 */
-	public function print_importmaps() {
-		// Directories to find JS files in, the setup ES6 import maps for
-		$directories = [
-			// namespace => path
-			''       => '/scripts',
-			'utils/' => '/scripts/utils',
-		];
-
-		$urls = [];
-		foreach ( $directories as $namespace => $path ) {
-			$files = glob( get_template_directory() . $path . '/*.js' );
-			foreach ( $files as $file ) {
-				$urls[ $namespace . basename( $file, '.js' ) ] = get_template_directory_uri() . $path . '/' . basename( $file );
-			}
-		}
-
-		$imports = [
-			'imports' => [
-				...$urls,
-			],
-		];
-		?>
-
-		<script type="importmap">
-			<?php echo json_encode( $imports ); ?>
-		</script>
-
-		<?php
 	}
 }
