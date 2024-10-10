ff130388c0c81e1363737c9360b0562356ef311e
rsm0128 <rsm0128@hotmail.com>
2024-10-09 23:33:46 -0400

    added purge feature

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -20,39 +20,46 @@ class Settings {
 	 */
 	public function init() {
 		add_action( 'admin_bar_menu', array( $this, 'add_purge_link' ), 1000 );
+		add_action( 'init', array( $this, 'purge_cache' ) );
 	}
 
+	/**
+	 * Adds purge link to adminbar menu.
+	 */
 	public function add_purge_link( $admin_bar ) {
 		if ( ! current_user_can( 'manage_options' ) ) {
 			return; // security check.
 		}
 
-		$current_url = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		$current_url = ( isset( $_SERVER['HTTPS'] ) ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 		// Parse the URL into components
-		$url_parts = parse_url($current_url);
-		
+		$url_parts = parse_url( $current_url );
+
 		// Parse existing query parameters
-		parse_str($url_parts['query'] ?? '', $query_params);
+		parse_str( $url_parts['query'] ?? '', $query_params );
 
 		// Add/modify the parameter
-		$query_params[$param_name] = $param_value;
+		$query_params['purge-wp-easy'] = '1';
 
-		// Build the new query string
-		$new_query = http_build_query($query_params);
-		
-		// Construct the new URL
-		$new_url = $url_parts['path'] . (empty($new_query) ? '' : '?' . $new_query);
-
-		$params           = array_merge( $_GET, array( 'purge_wp_easy' => '1' ) );
-		$new_query_string = http_build_query( $params );
+		$new_query = http_build_query( $query_params );
+		$new_url   = $url_parts['path'] . ( empty( $new_query ) ? '' : '?' . $new_query );
 
 		$admin_bar->add_menu(
 			array(
 				'id'    => 'wp-easy-purge-component-styles',
-				'title' => 'Purse Component Styles',
-				'link'  => 
+				'title' => 'Purge WP Easy',
+				'href'  => $new_url,
 			)
 		);
+	}
+
+	/**
+	 * Purge cache.
+	 */
+	public function purge_cache() {
+		if ( isset( $_GET['purge-wp-easy'] ) ) {
+			Utils::purge_component_styles();
+		}
 	}
 }
